50feb7542bc627caff5fa49edf896ca14a618172
RobinPC <sant4n4.1@gmail.com>
2026-09-11 19:37:52 -0500

    Agregando un servicio de mantenimiento de socio

---
+++ msvc_man_socio/src/main/java/com/robin/msvc_man_socio/MsvcManSocioApplication.java
@@ -0,0 +1,13 @@
+package com.robin.msvc_man_socio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcManSocioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcManSocioApplication.class, args);
+	}
+
+}
---
+++ msvc_man_socio/src/test/java/com/robin/msvc_man_socio/MsvcManSocioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.msvc_man_socio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcManSocioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
